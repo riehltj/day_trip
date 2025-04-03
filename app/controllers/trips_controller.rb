75dd545c83b919f9b2aba2fbf2dc3dcb6ec2dfc3
Tyler Riehl <tyler@tryboxedup.com>
2025-04-03 11:32:56 -0600

    Fixed font in dollar hover to be normal

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TripsController < ApplicationController
-  before_action :authenticate_user!, only: %i[index show new create]
+  before_action :authenticate_user!, only: %i[index show create]
   before_action :set_ride, only: %i[show new]
 
   def index
