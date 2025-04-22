db0f190a431ba2809a15f8f93921d650c3230dc5
Tyler Riehl <tyler@tryboxedup.com>
2025-04-22 09:35:16 -0600

    Added status to be an integer/enumerator

--- db/migrate/20250422152113_change_ride_status_to_int.rb
+++ db/migrate/20250422152113_change_ride_status_to_int.rb
@@ -2,16 +2,13 @@
 
 class ChangeRideStatusToInt < ActiveRecord::Migration[7.1]
   def up
-    change_table :rides, bulk: true do |t|
-      t.remove :status, type: :string
-      t.add :status, :integer, default: 0, null: false
-    end
+    change_column :rides, :status, :integer, using: 'status::integer', default: 0
   end
 
   def down
     change_table :rides, bulk: true do |t|
       t.remove :status, type: :integer
-      t.add :status, :string, default: 'open', null: false
+      t.add :status, :string
     end
   end
 end
