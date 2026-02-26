93d5febbd3b74604b5aabe6cb816304e693e3310
Tyler Riehl <tyler.riehl16@gmail.com>
2026-02-26 10:17:34 -0700

    fixed issues on user update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+    if update_resource(resource, account_update_params)
+      bypass_sign_in resource, scope: resource_name
+      set_flash_message :notice, :updated
+      redirect_to after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   protected
 
   def after_update_path_for(_resource)
